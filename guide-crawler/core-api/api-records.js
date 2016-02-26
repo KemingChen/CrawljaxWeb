dcefb870d7cc4bee483cca8723640f63df18ed08
KemingChen <believe75467@gmail.com>
2016-02-26 16:47:10 +0800

    完成 標記 Input 編號，並截圖

--- guide-crawler/core-api/api-records.js
+++ guide-crawler/core-api/api-records.js
@@ -3,7 +3,9 @@
 
     self.config = require('../config');
     self.webshot = require('webshot');
+    self.jsdom = require('jsdom').jsdom;
     self.path = require('path');
+
     module.exports = installRecordsApi;
 
     function installRecordsApi(router) {
@@ -43,16 +45,51 @@
         var domFilenameArray = self.config.RECORDS_PATH.getDirSync(dir + '/plugins/0/doms');
         domFilenameArray.forEach(function (filename) {
             var content = self.config.RECORDS_PATH.getFileSync(dir + '/plugins/0/doms/' + filename);
-            var imageFilename = self.path.join(dir, filename + ".png");
 
+            var imageFilename = self.path.join(dir, filename + ".png");
             result.result['states'][filename.split('.')[0]]['snapshot'] = './images/' + imageFilename;
 
             var output = self.path.join(self.config.IMAGE_PATH.value, imageFilename);
-            webshot(content, output, {siteType: 'html'}, function (err) {
+            var window = jsdom(content).defaultView;
+            var $ = require('jQuery')(window);
+
+            $("textarea, input:not([type]), input[type=text], input[type=password]")
+                .each(function (index) {
+                    $(this).attr('placeholder', (index + 1));
+                });
+
+            webshot(window.document.documentElement.outerHTML, output, {
+                siteType: 'html',
+                defaultWhiteBackground: true,
+                shotSize: {
+                    width: 'window',
+                    height: 'all'
+                },
+                customCSS: getCustomCSS()
+            }, function (err) {
                 err && console.log(err);
             });
         });
 
         res.json(result);
+
+        function getCustomCSS() {
+            return '\
+                textarea,\
+                input:not([type]),\
+                input[type=text],\
+                input[type=password] {\
+                    border: 3px solid red;\
+                    font-size: 16px !important;\
+                    margin-bottom: 1px !important;\
+                }\
+                textarea::-webkit-input-placeholder,\
+                input:not([type])::-webkit-input-placeholder,\
+                input[type=text]::-webkit-input-placeholder,\
+                input[type=password]::-webkit-input-placeholder {\
+                    color: red;\
+                    font-weight: bolder;\
+                }';
+        }
     }
 })();
